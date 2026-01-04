ca8d56788419578fb9ae34ceeba2350aa6cc98b3
Zherdev Igor <cardinal3300@.com>
2026-01-04 16:10:50 +0300

    Сделана структура (скелет) проекта, подключен DRF, настроен CORS, готов юзер под Telegram

---
+++ users/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    telegram_chat_id = models.CharField(
+        max_length=50,
+        blank=True,
+        null=True,
+        verbose_name="Telegram chat ID"
+    )
