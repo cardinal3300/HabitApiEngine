1db79addae716762e7ffc336674e8e89c74ff1df
Zherdev Igor <cardinal3300@.com>
2026-02-04 21:02:15 +0300

    начало настройки перед итоговой работой

--- config/urls.py
+++ config/urls.py
@@ -1,31 +1,32 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
-
 schema_view = get_schema_view(
     openapi.Info(
-        title='Snippets API',
-        default_version='v1',
-        description='Test description',
-        terms_of_service='https://www.google.com/policies/terms/',
-        contact=openapi.Contact(email='contact@snippets.local'),
-        license=openapi.License(name='BSD License'),
+        title="Snippets API",
+        default_version="v1",
+        description="HabitApiEngine description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('users.urls', namespace='users')),
-    path('api/', include('habits.urls')),
+    path("admin/", admin.site.urls),
+    path("api/", include("users.urls", namespace="users")),
+    path("api/", include("habits.urls")),
     path(
-        'api/swagger/',
-        schema_view.with_ui('swagger', cache_timeout=0),
-        name='schema-swagger-ui',
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
     ),
-    path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path(
+        "redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
 ]
